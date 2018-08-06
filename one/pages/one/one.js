0d85141d832627f8f20749d89e7e9e375c83e1d1
angelyu <321587153@qq.com>
2018-08-06 20:15:44 +0800

    小程序

---
+++ one/pages/one/one.js
@@ -0,0 +1,85 @@
+// pages/one/one.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    videoList:[
+      {
+        video:'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
+        detail:'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        auto: true
+      },
+      
+      {
+        video: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
+        detail: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        auto: false
+      }
+    ]
+  },
+  play(e){
+    console.log(e.detail.current)
+    var index = e.detail.current;
+    this.data.videoList[index].auto = true;
+    this.setData({
+      videoList: this.data.videoList
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
